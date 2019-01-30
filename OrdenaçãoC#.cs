f12fcb5007f1795cb0b5f7f662b14ad8012400cb
Wellington Reis <wellington.costa.97@gmail.com>
2019-01-30 19:56:31 -0200

    Create OrdenaçãoC#.cs

---
+++ OrdenaçãoC#.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Class1
+{
+	public int Class1()
+	{
+        int[] vetor = { 9, 5, 7, 2 };
+        int aux = 0;
+        for (int w = 0; w < vetor.Length; w++)
+        {
+            for (int r = 0; r < vetor.Length; r++)
+            {
+                if (vetor[w] < vetor[r])
+                {
+                    aux = vetor[i];
+                    vetor[w] = vetor[r];
+                    vetor[r] = aux;
+                }
+            }
+        }
+        for (int k = 0; k < vetor.Length; k++)
+        {
+            Console.WriteLine(vetor[k]);
+        }
+
+        Console.ReadKey();
+    }
+}
